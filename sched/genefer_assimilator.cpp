0d7248cba735bd118a7f896aeb3391ffe84cf55c
Roman Trunov <stream@proxyma.ru>
2019-05-13 11:17:20 +0300

    Genefer validator and assimilator for GFN-16 MEGA (LLR) project

--- sched/genefer_assimilator.cpp
+++ sched/genefer_assimilator.cpp
@@ -15,10 +15,6 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with BOINC.  If not, see <http://www.gnu.org/licenses/>.
 
-// A sample assimilator that:
-// 1) if success, copy the output file(s) to a directory
-// 2) if failure, append a message to an error log
-
 #include <vector>
 #include <string>
 #include <cstdlib>
@@ -29,43 +25,90 @@
 #include "sched_msgs.h"
 #include "validate_util.h"
 #include "sched_config.h"
+#include <inttypes.h>
 
 using std::vector;
 using std::string;
 
 #define OUTPUT_DIR "genefer_results"
 
-int write_error(char* p) {
-    static FILE* f = 0;
-    if (!f) {
-        f = fopen(config.project_path(OUTPUT_DIR "/errors"), "a");
-        if (!f) return ERR_FOPEN;
-    }
-    fprintf(f, "%s", p);
-    fflush(f);
-    return 0;
-}
+int get_residue_strdup(RESULT &result, const char *infile, char* &data, unsigned & EXP, uint64_t & B);
+unsigned get_llr_version(const char *text);
+extern bool use_llr;
 
-int get_residue_strdup(RESULT &result, const char *infile, char* &data, unsigned & EXP, unsigned & N);
-
-int assimilate_handler_init(int argc, char** argv) {
+int assimilate_handler_init(int argc, char** argv)
+{
     // handle project specific arguments here
-    (void) argc; (void) argv;
-    return 0;
+    return validate_handler_init(argc, argv);
 }
 
-void assimilate_handler_usage() {
+void assimilate_handler_usage(void)
+{
     // describe the project specific arguments here
     //fprintf(stderr,
     //    "    Custom options:\n"
     //    "    [--project_option X]  a project specific option\n"
     //);
+    validate_handler_usage();
+}
+
+static int copy_result_files(RESULT &result, FILE *fout)
+{
+    char buf[1024];
+    vector<OUTPUT_FILE_INFO> files;
+    int retval;
+
+    retval = get_output_file_infos(result, files);
+    if (retval)
+    {
+        log_messages.printf(MSG_CRITICAL,
+            "[RESULT#%lu %s] can't get output filenames\n",
+            result.id, result.name
+        );
+        return retval;
+    }
+
+    int nFiles = files.size();
+    for (int i = 0; i < nFiles; i++)
+    {
+        OUTPUT_FILE_INFO& fi = files[i];
+        FILE *f = fopen(fi.path.c_str(), "rt");
+        if (f == NULL)
+        {
+            if (result.validate_state != VALIDATE_STATE_VALID)
+                continue;
+            log_messages.printf(MSG_CRITICAL,
+                "[RESULT#%lu %s] can't open '%s'%s\n",
+                result.id, result.name,
+                fi.path.c_str(),
+                (result.validate_state == VALIDATE_STATE_VALID ? "" : ", ignored for invalid result")
+                );
+            return ERR_FOPEN;
+        }
+        if (fprintf(fout, "[%s]\n", fi.name.c_str()) < 0)
+            retval = ERR_FWRITE;
+        while (!retval && fgets(buf, sizeof(buf)-1, f))
+        {
+            int sz = strlen(buf);
+            // force \n on corrupted lines
+            if (sz && buf[sz-1] != '\n')
+            {
+                buf[sz] = '\n';
+                buf[sz+1] = 0;
+            }
+            if (fputs(buf, fout) == EOF)
+                retval = ERR_FWRITE;
+        }
+        fclose(f);
+    }
+
+    return retval;
 }
 
 int assimilate_handler(WORKUNIT& wu, vector<RESULT>& results, RESULT& canonical_result)
 {
     int retval;
-    char buf[1024];
+    char tsbuf[128];
 
     static bool once;
     if (!once)
@@ -83,7 +126,7 @@ int assimilate_handler(WORKUNIT& wu, vector<RESULT>& results, RESULT& canonical_
         if (nFiles != 1)
         {
             log_messages.printf(MSG_CRITICAL,
-                "[RESULT#%lu %s] unexpected number of input files (%d)\n",
+                "[RESULT#%lu %s] unexpected number of files (%d)\n",
                 canonical_result.id, canonical_result.name, nFiles
             );
             return ERR_READ;
@@ -91,8 +134,9 @@ int assimilate_handler(WORKUNIT& wu, vector<RESULT>& results, RESULT& canonical_
 
         OUTPUT_FILE_INFO& fi = output_files[0];
         char *res = NULL;
-        unsigned N, EXP;
-        retval = get_residue_strdup(canonical_result, fi.path.c_str(), res, EXP, N);
+        unsigned EXP;
+        uint64_t B;
+        retval = get_residue_strdup(canonical_result, fi.path.c_str(), res, EXP, B);
         if (retval)
             return retval;
         unsigned EXP_SCALE;
@@ -100,11 +144,12 @@ int assimilate_handler(WORKUNIT& wu, vector<RESULT>& results, RESULT& canonical_
         {
         case  8192: EXP_SCALE = 13; break;
         case 16384: EXP_SCALE = 14; break;
+        case 65536: EXP_SCALE = 16; break;
         default:    EXP_SCALE = 99; break;
         }
         time_t now = time(NULL);
-        strftime(buf, sizeof(buf), "%F", gmtime(&now));
-        const char *output_path = config.project_path(OUTPUT_DIR "/gfn%u_%s.csv", EXP_SCALE, buf);
+        strftime(tsbuf, sizeof(tsbuf), (wu.transitioner_flags & TRANSITION_NO_NEW_RESULTS ? "%Y-%m-cancelled" : "%F"), gmtime(&now));
+        const char *output_path = config.project_path(OUTPUT_DIR "/gfn%u_%s.csv", EXP_SCALE, tsbuf);
         FILE *f = fopen(output_path, "at");
         if (f == NULL)
         {
@@ -117,7 +162,7 @@ int assimilate_handler(WORKUNIT& wu, vector<RESULT>& results, RESULT& canonical_
         }
 
         bool err = false;
-        if (fprintf(f, "%u,%u,%s", EXP, N, res) < 0)
+        if (fprintf(f, "%u,%" PRIu64 ",%s", EXP, B, res) < 0)
             err = true;
         for (unsigned i=0; i < results.size(); i++)
         {
@@ -126,6 +171,11 @@ int assimilate_handler(WORKUNIT& wu, vector<RESULT>& results, RESULT& canonical_
             {
                 if (fprintf(f, ",%ld,%ld,%ld,%d", r.userid, r.hostid, r.app_version_id, r.received_time) < 0)
                     err = true;
+                if (use_llr)
+                {
+                    if (fprintf(f, ",%u", get_llr_version(r.stderr_out)) < 0)
+                        err = true;
+                }
             }
         }
         if (fprintf(f, "\n") < 0)
@@ -143,11 +193,59 @@ int assimilate_handler(WORKUNIT& wu, vector<RESULT>& results, RESULT& canonical_
             );
             return ERR_FWRITE;
         }
+
+        // Keep extra archive of full LLR results
+        if (use_llr)
+        {
+            FILE *fout;
+
+            output_path = config.project_path(OUTPUT_DIR "/residues-gfn%u_%s.txt", EXP_SCALE, tsbuf);
+            fout = fopen(output_path, "at");
+            if (fout == NULL)
+            {
+                log_messages.printf(MSG_CRITICAL,
+                    "[RESULT#%lu %s] cannot open output file '%s'\n",
+                    canonical_result.id, canonical_result.name, output_path
+                );
+                return ERR_FOPEN;
+            }
+
+            retval = 0;
+            for (unsigned i = 0; i < results.size() && !retval; i++)
+                retval = copy_result_files(results[i], fout);
+
+            if (fclose(fout) < 0)
+                retval = ERR_FWRITE;
+
+            if (retval)
+            {
+                log_messages.printf(MSG_CRITICAL,
+                    "[RESULT#%lu %s] cannot write to output file '%s'\n",
+                    canonical_result.id, canonical_result.name, output_path
+                );
+                return retval;
+            }
+        }
     }
     else
     {
-        sprintf(buf, "%s: 0x%x\n", wu.name, wu.error_mask);
-        return write_error(buf);
+        const char *output_path = config.project_path(OUTPUT_DIR "/errors");
+        FILE *f = fopen(output_path, "at");
+        if (f == NULL)
+        {
+            log_messages.printf(MSG_CRITICAL, "cannot open output file '%s'\n", output_path);
+            return ERR_FOPEN;
+        }
+        bool err = false;
+        if (fprintf(f, "%s: 0x%x\n", wu.name, wu.error_mask) < 0)
+            err = true;
+        if (fclose(f) < 0)
+            err = true;
+        if (err)
+        {
+            log_messages.printf(MSG_CRITICAL, "cannot write to output file '%s'\n", output_path);
+            return ERR_FWRITE;
+        }
     }
     return 0;
 }
--- sched/genefer_validator.cpp
+++ sched/genefer_validator.cpp
@@ -5,36 +5,64 @@
 #include "validate_util.h"
 #include "validate_util2.h"
 #include "validator.h"
+#include <inttypes.h>
 
 using std::string;
 using std::vector;
 
+bool use_llr;
+
+#define GFN16MEGA_B_OFFSET           1814570322693370
+
 int validate_handler_init(int argc, char **argv)
 {
-    (void) argc; (void) argv;
+    int i;
 
+    for (i = 1; i < argc; i++)
+    {
+        if (strcmp(argv[i], "--llr") == 0)
+            use_llr = true;
+        else
+        {
+            fprintf(stderr, "Unknown option '%s'\n", argv[i]);
+            return -1;
+        }
+    }
     return 0;
 }
 
 void validate_handler_usage(void)
 {
     // describe the project specific arguments here
+    fprintf(stderr,
+        "    Custom options:\n"
+        "    [--llr]                    parse LLR output files (default: Genefer)\n"
+    );
 }
 
-static int get_residue_from_fp(RESULT &result, const char *infile, char* &data, unsigned &RET_EXP, unsigned &RET_N, FILE *in)
+static int get_residue_from_fp(RESULT &result, const char *infile, char* &data, unsigned &RET_EXP, uint64_t &RET_B, FILE *in)
 {
     static bool once;
     static regex_t reg_composite, reg_prime;
+    static regex_t reg_compo_llr, reg_prime_llr;
     if (!once)
     {
+        // Genefer:
         // 13100008^8192+1 is composite. (RES=079b7b164cd29f71) (58305 digits) (err = 0.0000) (time = 0:00:09) 00:56:22
         // 13199802^8192+1 is a probable prime. (58332 digits) (err = 0.0044) (time = 0:01:32) 22:04:35
+        // LLR:
+        // 1814570322703210^65536+1 is not prime.  RES64: 34256CFC2ED1BF8E.  OLD64: 9C7046F48C753EA7  Time : 10446.820 sec.
+        // 143946294^8192+1 is prime! (66832 decimal digits)  Time : 376.664 sec.
         static const char 
             pattern_composite[] = "^([0-9]+)\\^([0-9]+)\\+1 is composite\\. \\(RES=([0-9a-fA-F]{16})\\) \\([0-9]+ digits\\) \\(err = [0-9]",
+            pattern_compo_llr[] = "^([0-9]+)\\^([0-9]+)\\+1 is not prime\\.  RES64: ([0-9a-fA-F]{16})\\.  OLD64: ([0-9a-fA-F]{16})  Time : [0-9]+",
+            pattern_prime_llr[] =" ^([0-9]+)\\^([0-9]+)\\+1 is prime! \\([0-9]+ decimal digits\\)  Time : [0-9]+",
             pattern_prime[]     = "^([0-9]+)\\^([0-9]+)\\+1 is a probable prime\\. \\([0-9]+ digits\\) \\(err = [0-9]";
 
         once = true;
         if (regcomp(&reg_composite, pattern_composite, REG_EXTENDED | REG_ICASE) ||
+            regcomp(&reg_compo_llr, pattern_compo_llr, REG_EXTENDED | REG_ICASE) ||
+            regcomp(&reg_prime_llr, pattern_prime_llr, REG_EXTENDED | REG_ICASE) ||
             regcomp(&reg_prime,     pattern_prime,     REG_EXTENDED | REG_ICASE)
            )
         {
@@ -47,55 +75,53 @@ static int get_residue_from_fp(RESULT &result, const char *infile, char* &data, 
     }
 
     int retval = ERR_OPENDIR;   // transient error to diagnose later
-    int size;
     char inbuf[1024];
-    if ((size = fread(inbuf, 1, sizeof(inbuf), in)) <= 0)
+
+read_again:
+    if (fgets(inbuf, sizeof(inbuf), in) == NULL)
     {
+        if (data)
+            return 0;
+
         log_messages.printf(MSG_CRITICAL,
-            "[RESULT#%lu %s] error reading input file '%s'\n",
-            result.id, result.name, infile
-        );
-        return retval;
-    }
-    if (size >= (int)sizeof(inbuf))
-    {
-        log_messages.printf(MSG_CRITICAL,
-            "[RESULT#%lu %s] input file '%s' is too big\n",
+            "[RESULT#%lu %s] no signatures found in input file '%s'\n",
             result.id, result.name, infile
         );
         return retval;
     }
 
-    {
     char *p;
-    inbuf[size] = 0;
     while ((p = strpbrk(inbuf, "\r\n")) != NULL)  // CR-LF to spaces
         *p = ' ';
+    for (int sz=strlen(inbuf); sz;)  // remove trailing spaces
+    {
+        if (inbuf[--sz] != ' ') break;
+        inbuf[sz] = 0;
     }
 
     #define PMATCH_COUNT 5
     bool prime;
     regmatch_t pmatch[PMATCH_COUNT];
 
-    if (regexec(&reg_composite, inbuf, PMATCH_COUNT, pmatch, 0) == 0)
+    if (regexec((use_llr ? &reg_compo_llr : &reg_composite), inbuf, PMATCH_COUNT, pmatch, 0) == 0)
     {
         prime = false;
     }
-    else if (regexec(&reg_prime, inbuf, PMATCH_COUNT, pmatch, 0) == 0)
+    else if (regexec((use_llr ? &reg_prime_llr : &reg_prime), inbuf, PMATCH_COUNT, pmatch, 0) == 0)
     {
         prime = true;
     }
     else
     {
         log_messages.printf(MSG_CRITICAL,
-            "[RESULT#%lu %s] input data not parsed: '%s'\n",
+            "[RESULT#%lu %s] input line not parsed: '%s'\n",
             result.id, result.name, inbuf
         );
-        return retval;
+        goto read_again;
     }
 
     // 0 - whole string
-    // 1 - N
+    // 1 - B
     // 2 - exponent
     // 3 - residue if not prime
 
@@ -108,36 +134,41 @@ static int get_residue_from_fp(RESULT &result, const char *infile, char* &data, 
         goto fail_name;
     file_exp = atoi(pp+3);
     pp = strchr(pp, '_');
+    if (pp && memcmp(pp, "_mega_", 6) == 0)  // gfn16_mega_
+        pp += 5;
     if (!pp || !isdigit(pp[1]))
     {
     fail_name:
         log_messages.printf(MSG_CRITICAL,
-            "[RESULT#%lu %s] cannot guess N from input file name '%s'\n",
+            "[RESULT#%lu %s] cannot guess B from input file name '%s'\n",
             result.id, result.name, infile
         );
         return retval;
     }
-    int expected_value = atoi(pp+1);
-    int n = atoi(inbuf + pmatch[1].rm_so);
+    int64_t expected_value = atoll(pp+1);
+    int64_t n = atoll(inbuf + pmatch[1].rm_so);
+    if (file_exp == 16)
+        expected_value += GFN16MEGA_B_OFFSET;
     if (n != expected_value)
     {
         log_messages.printf(MSG_CRITICAL,
-            "[RESULT#%lu %s] bad N %d, expected %d\n",
+            "[RESULT#%lu %s] bad B %" PRIi64 ", expected %" PRIi64 "\n",
             result.id, result.name, n, expected_value
         );
         return retval;
     }
-    RET_N = n;
+    RET_B = n;
 
     // exponent check
     switch (file_exp)
     {
     case 13: expected_value = 8192;   break;
     case 14: expected_value = 16384;  break;
+    case 16: expected_value = 65536;  break;
     default:
         log_messages.printf(MSG_CRITICAL,
             "[RESULT#%lu %s] cannot guess exponent from N %d\n",
-            result.id, result.name, n
+            result.id, result.name, file_exp
         );
         return retval;
     }
@@ -145,33 +176,46 @@ static int get_residue_from_fp(RESULT &result, const char *infile, char* &data, 
     if (n != expected_value)
     {
         log_messages.printf(MSG_CRITICAL,
-            "[RESULT#%lu %s] bad exponent %d, expected %d\n",
+            "[RESULT#%lu %s] bad exponent %" PRIi64 ", expected %" PRIi64 "\n",
             result.id, result.name, n, expected_value
         );
         return retval;
     }
     RET_EXP = n;
 
+    const char *curdata;
     if (prime)
-    {
-        data = strdup("PRIME");
-    }
+        curdata = "PRIME";
     else
     {
         inbuf[pmatch[3].rm_eo] = 0;
-        data = strdup(inbuf + pmatch[3].rm_so);
-        for (char *p = data; *p; p++)
+        curdata = inbuf + pmatch[3].rm_so;
+    }
+    if (data == NULL)
+    {
+        data = strdup(curdata);
+        for (p = data; *p; p++)
             *p = toupper(*p);
     }
-    return 0;
+    else if (strcmp(data, curdata))
+    {
+        log_messages.printf(MSG_CRITICAL,
+            "[RESULT#%lu %s] multiple mismatched residues '%s' and '%s'\n",
+            result.id, result.name,
+            data, curdata
+        );
+        return retval;
+    }
+    goto read_again;
 }
 
-int get_residue_strdup(RESULT &result, const char *infile, char* &data, unsigned &RET_EXP, unsigned &RET_N)
+int get_residue_strdup(RESULT &result, const char *infile, char* &data, unsigned &RET_EXP, uint64_t &RET_B)
 {
     FILE *in;
 
     int retval;
 
+    data = NULL;
     retval = try_fopen(infile, in, "rt");
     if (retval)
     {
@@ -182,11 +226,40 @@ int get_residue_strdup(RESULT &result, const char *infile, char* &data, unsigned
     }
     else
     {
-        retval = get_residue_from_fp(result, infile, data, RET_EXP, RET_N, in);
+        retval = get_residue_from_fp(result, infile, data, RET_EXP, RET_B, in);
         fclose(in);
     }
 
     return retval;
+}
+
+unsigned get_llr_version(const char *text)
+{
+    static const char pattern[] = "\nLLR Program - Version ([0-9]+)\\.([0-9]+)\\.([0-9]+),";
+    static bool once;
+    static regex_t reg;
+
+    if (!once)
+    {
+        once = true;
+        if (regcomp(&reg, pattern, REG_EXTENDED | REG_ICASE))
+        {
+            log_messages.printf(MSG_CRITICAL,
+                "%s: regex compile failed\n", __FUNCTION__
+            );
+            exit(1);
+        }
+    }
+
+    regmatch_t pmatch[PMATCH_COUNT];
+    unsigned version = 0;
+    if (regexec(&reg, text, PMATCH_COUNT, pmatch, 0) == 0)
+    {
+        int i;
+        for (i = 1; i <= 3; i++)
+            version = version * 100 + atoi(text + pmatch[i].rm_so);
+    }
+    return version;
 }
 
 int init_result(RESULT& result, void*& data)
@@ -228,15 +301,16 @@ int init_result(RESULT& result, void*& data)
         }
 
         char *res = NULL;
-        unsigned dummy_exp, dummy_n;
-        retval = get_residue_strdup(result, fi.path.c_str(), res, dummy_exp, dummy_n);
+        unsigned dummy_exp;
+        uint64_t dummy_b;
+        retval = get_residue_strdup(result, fi.path.c_str(), res, dummy_exp, dummy_b);
 //      printf("Got residue: '%s'\n", res);
         char *full_res = NULL;
         if (res)
         {
             full_res = (char*) malloc(strlen(res) + 64);
             if (full_res)
-                sprintf(full_res, "%u %u %s", dummy_exp, dummy_n, res);
+                sprintf(full_res, "%u %" PRIu64 " %s", dummy_exp, dummy_b, res);
             free(res);
         }
 //      printf("Got data: '%s'\n", full_res);
@@ -253,7 +327,6 @@ int compare_results(
     bool& match
     )
 {
-    (void) r2;
     if (data1 == NULL || data2 == NULL)
     {
         log_messages.printf(MSG_CRITICAL,
@@ -267,8 +340,8 @@ int compare_results(
     if (!match)
     {
         log_messages.printf(MSG_CRITICAL, "Validation error:\n");
-        log_messages.printf(MSG_CRITICAL, "  [RESULT#%lu %s U%ld H%ld] %s\n", r1.id, r1.name, r1.userid, r1.hostid, (char*)data1);
-        log_messages.printf(MSG_CRITICAL, "  [RESULT#%lu %s U%ld H%ld] %s\n", r2.id, r2.name, r2.userid, r2.hostid, (char*)data2);
+        log_messages.printf(MSG_CRITICAL, "  [RESULT#%lu %s U%-4ld H%-4ld] %s\n", r1.id, r1.name, r1.userid, r1.hostid, (char*)data1);
+        log_messages.printf(MSG_CRITICAL, "  [RESULT#%lu %s U%-4ld H%-4ld] %s\n", r2.id, r2.name, r2.userid, r2.hostid, (char*)data2);
     }
 
     return 0;
